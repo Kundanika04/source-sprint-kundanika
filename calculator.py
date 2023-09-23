3cf4011dd79d731792dd9abd5cc2964dfeb20597
Kundanika04 <145657916+Kundanika04@users.noreply.github.com>
2023-09-23 14:51:42 +0530

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,13 @@
+a,b = map(int, input("Enter the two space separated integers:\n").split())
+s=input("Enter the operator:\n")
+if(s=='+'):
+    print(a+b)
+elif(s=='-'):
+    print(a-b)
+elif(s=='/'):
+    print(a/b)
+elif(s=='*'):
+    print(a*b)
+else:
+    print("Enter the correct operator.\n")
+print("Thank you.\n")
